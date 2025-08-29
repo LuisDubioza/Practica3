c9561f1178f0c5c89c2af5866575b8a4176dba59
LuisDubioza <luisriv@ciencias.unam.mx>
2025-08-28 18:13:36 -0600

    agregue practica 1

--- src/Practica3.java
+++ src/Practica3.java
@@ -1,5 +1,11 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+public class Practica3 {
+    public static void main(String[] args) {
+
+        System.out.println("\nBienvenido a Trash The Dragon \n");
+        
     }
+    
+    
 }
